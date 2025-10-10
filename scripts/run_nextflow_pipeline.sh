ef566ae20b79ecef9ce98763eff7c9cc1f4e7746
nbratset <natalie.bratset@colorado.edu>
2025-10-10 13:01:54 -0600

    modified run indir and outdir

--- scripts/run_nextflow_pipeline.sh
+++ scripts/run_nextflow_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH --job-name=nextflow_help # Job name
+#SBATCH --job-name=nextflow_run # Job name
 #SBATCH --mail-type=ALL # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=natalie.bratset@colorado.edu # Where to send mail
 #SBATCH --nodes=1 # Run on a single node
@@ -16,27 +16,25 @@ echo "$dt"
 
 
 path_to_venv=$HOME/projects/CSCI6118/micromamba/envs/
+
 source ~/.bashrc
 micromamba activate ${path_to_venv}nextflow_env
 
 indir=/scratch/Shares/biotracker_csci6118/
-
 echo $indir
 
 # --- Execution ---
-nextflow help
-
-
-# nextflow run nf-core/taxprofiler -r 1.1.0 -profile conda \
-#   --input /scratch/Shares/biotracker_csci6118/samplesheet.csv \
-#   --databases /scratch/Shares/biotracker_csci6118/database.csv \
-#   --outdir /scratch/Shares/biotracker_csci6118/results \
-#   --perform_shortread_qc \
-#   --perform_shortread_hostremoval \
-#   --hostremoval_reference /scratch/Shares/biotracker_csci6118/GCF_000819615.1_ViralProj14015_genomic.fna.gz \
-#   --perform_runmerging \
-#   --save_runmerged_reads \
-#   --run_metaphlan
+# nextflow help
+nextflow run nf-core/taxprofiler -r 1.1.0 -profile conda \
+  --input ${indir}samplesheet.csv \
+  --databases ${indir}database.csv \
+  --outdir ${indir}nextflow_results \
+  --perform_shortread_qc \
+  --perform_shortread_hostremoval \
+  --hostremoval_reference ${indir}GCF_000819615.1_ViralProj14015_genomic.fna.gz \
+  --perform_runmerging \
+  --save_runmerged_reads \
+  --run_metaphlan
 
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
