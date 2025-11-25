677be70404126371bcd0af0277fd7cb28644a546
arya-arga9502 <arga9502@colorado.edu>
2025-11-25 12:22:46 -0700

    differential abundance analysis

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ import pandas as pd
 from diversity import compute_alpha, compute_beta
 from ordination import run_pcoa
 from taxonomy_plot import barplot_taxa_facet_fill
+from randomforest import run_rf_multiclass
 import matplotlib.pyplot as plt
 import seaborn as sns
+from skbio.stats.composition import ancom
 
 #Save results
 OUTDIR = "results"
@@ -79,6 +81,7 @@ alpha_df
 alpha_df = alpha_df.join(metadata[["Condition", "Location"]])
 alpha_df.to_csv(f"{OUTDIR}/alpha_diversity.csv")
 
+##Richness
 plt.figure(figsize=(10,6))
 g = sns.catplot(
     data=alpha_df,
@@ -112,7 +115,7 @@ plt.tight_layout()
 plt.savefig(f"{OUTDIR}/alpha_diversity_plot_obs.png", dpi=300)
 plt.close()
 
-
+#Shannon
 plt.figure(figsize=(10,6))
 g = sns.catplot(
     data=alpha_df,
@@ -146,7 +149,44 @@ plt.tight_layout()
 plt.savefig(f"{OUTDIR}/alpha_diversity_plot_shannon.png", dpi=300)
 plt.close()
 
+#Random Forest
+dataMatrix = otu.copy()
+dataMatrix["Condition"] = metadata["Condition"]
 
+cols = ["Condition"] + [c for c in dataMatrix.columns if c != "Condition"]
+dataMatrix = dataMatrix[cols]
 
+auc_fig, confusion_mat, patient_preds = run_rf_multiclass(
+    dataMatrix,
+    class_col="Condition",
+    auc_outfile=f"{OUTDIR}/rf_auc_curve.png",
+    report_txt=f"{OUTDIR}/rf_report.txt"
+)
 
+#Differential abundance
+metadata = metadata[metadata["Condition"] != "Patient"]
+metadata.loc[metadata["Condition"] == "Ulcerative colitis", "Condition"] = "Disease"
+metadata.loc[metadata["Condition"] == "Crohn's disease", "Condition"] = "Disease"
+group = metadata["Condition"]
+otu_clean = otu.loc[metadata.index]
+otu_clean = otu_clean + 1e-6
+
+ancom_df, percentile_df = ancom(otu_clean, group)
+sig_taxa = ancom_df[ancom_df['Signif']].index
+medians= percentile_df[50.0].loc[sig_taxa]
+medians['Diff'] = medians['Healthy control'] - medians['Disease']
+
+top_diff = medians['Diff'].abs().sort_values(ascending=False).head(20)
+top_taxa = medians.loc[top_diff.index]
+
+plt.figure(figsize=(10, 4))
+plt.bar(top_taxa.index, top_taxa['Diff'])
+plt.axhline(0, color='grey', linewidth=0.8, linestyle='--')
+plt.ylabel('Median Abundance Difference\n(Disease - Healthy control)')
+plt.xlabel('Taxon')
+plt.title('Top 10 Significant Taxa: Median Abundance Difference')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.savefig(f"{OUTDIR}/differential_abundance.png", dpi=300)
+plt.close()
 
