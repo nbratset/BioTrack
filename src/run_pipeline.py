f7a40dff7f0bb1436c49c3febccc6b64f05084ca
nbratset <nbratset@gmail.com>
2025-11-29 12:08:43 -0700

    added patient and report args

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -6,24 +6,33 @@ import os
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Run microbiome analysis pipeline"
-    )
+    parser = argparse.ArgumentParser(prog='BioTrack',
+                                     description="Run microbiome analysis pipeline")  # noqa
 
-    parser.add_argument(
-        "--location",
-        help="Location to limit the analysis to (optional)"
-    )
+    parser.add_argument("--location",
+                        help="Location to limit the analysis to (optional)")
 
-    parser.add_argument(
-        "--otu_file", required=True,
-        help="Path to microbial abundance table"
-    )
+    parser.add_argument("--otu_file",
+                        required=True,
+                        help="Path to microbial abundance table")
 
-    parser.add_argument(
-        "--metadata_file", required=True,
-        help="Path to metadata file"
-    )
+    parser.add_argument("--metadata_file",
+                        required=True,
+                        help="Path to metadata file")
+
+    parser.add_argument('-r',
+                        '--report',
+                        type=str,
+                        help='Build a report document (optional).',
+                        default=False,
+                        required=False)
+
+    parser.add_argument('-p',
+                        '--patient_id',
+                        type=str,
+                        default='',
+                        help='Patient name or ID for report (optional).',
+                        required=False)
 
     args = parser.parse_args()
 
