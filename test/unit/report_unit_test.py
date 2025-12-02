326f1908fbf833df4d72e4e1dc242a9207e9fc56
nbratset <nbratset@gmail.com>
2025-12-01 22:13:39 -0700

    fix pycodestyle

--- test/unit/report_unit_test.py
+++ test/unit/report_unit_test.py
@@ -14,28 +14,31 @@ class TestReport(unittest.TestCase):
         alpha_file = 'results/alpha_diversity.csv'
         beta_file = 'results/beta_diversity_coords.csv'
         diff_file = 'results/differential_abundance_top_20.csv'
-        df1, df2, df3, df4 = app.parse_csvs(taxa_file, alpha_file, beta_file, diff_file)
+        df1, df2, df3, df4 = app.parse_csvs(taxa_file,
+                                            alpha_file,
+                                            beta_file,
+                                            diff_file)
         # I'll test for sys.exit(0) here if you have a bad file.
         self.assertEqual(app.get_patient('bad.csv',
                                          alpha_file,
                                          beta_file,
                                          diff_file),
-                                         'Missing Taxa/OTU File')
+                                        'Missing Taxa/OTU File')
         self.assertEqual(app.get_patient(taxa_file,
                                          'bad.csv',
                                          beta_file,
                                          diff_file),
-                                         'Missing alpha diversity File')
+                                        'Missing alpha diversity File')
         self.assertEqual(app.get_patient(taxa_file,
                                          alpha_file,
                                          'bad.csv',
                                          diff_file),
-                                         'Missing beta diversity File')
+                                        'Missing beta diversity File')
         self.assertEqual(app.get_patient(taxa_file,
                                          alpha_file,
                                          beta_file,
                                          'bad.csv'),
-                                         'Missing Differential Abundance File')  # noqa
+                                        'Missing Differential Abundance File')  # noqa
 
     def test_get_patient(self):
         alpha_file = 'results/alpha_diversity.csv'
