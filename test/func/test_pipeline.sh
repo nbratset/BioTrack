1e5a28e98563dd1a8943a62db21acda94d73cc6d
nbratset <nbratset@gmail.com>
2025-12-01 21:42:43 -0700

    fixed test names

--- test/func/test_pipeline.sh
+++ test/func/test_pipeline.sh
@@ -8,14 +8,14 @@ run test_bad_otu_file python src/app.py -n 'John Doe' --taxa_file input_data/bad
 assert_exit_code 0
 assert_in_stdout "Missing Taxa/OTU File"
 
-run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --alpha_file input_data/bad_file.csv
+run test_bad_alpha_file python src/run_pipeline.py -n 'John Doe' --alpha_file input_data/bad_file.csv
 assert_exit_code 0
 assert_in_stdout "Missing alpha diversity File"
 
-run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --beta_file input_data/bad_file.csv
+run test_bad_beta_file python src/run_pipeline.py -n 'John Doe' --beta_file input_data/bad_file.csv
 assert_exit_code 0
 assert_in_stdout "Missing beta diversity File"
 
-run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --diff_file input_data/bad_file.csv
+run test_bad_diff_file python src/run_pipeline.py -n 'John Doe' --diff_file input_data/bad_file.csv
 assert_exit_code 0
 assert_in_stdout "Missing Differential Abundance File"
