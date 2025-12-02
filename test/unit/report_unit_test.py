2e125f7188adb14c825621090e939b7fdcf5f3c1
nbratset <nbratset@gmail.com>
2025-12-01 22:09:37 -0700

    added better report unit test outputs

--- test/unit/report_unit_test.py
+++ test/unit/report_unit_test.py
@@ -9,38 +9,43 @@ import app
 
 
 class TestReport(unittest.TestCase):
-    def test_import(self):
-        # I want this function to import 3 csvs and export them as dataframes
-        # I want it to export as 3 different dataframes
-        # taxa_file, alpha_file, beta_file will come from commandline
-        # import input_data/otu.csv
-        # import results/alpha_diversity.csv
-        # import results/beta_diversity_coords.csv
+    def test_parse_csvs(self):
         taxa_file = 'input_data/otu.csv'
         alpha_file = 'results/alpha_diversity.csv'
         beta_file = 'results/beta_diversity_coords.csv'
-        df1, df2, df3 = app.parse_csvs(taxa_file, alpha_file, beta_file)
-        self.assertEqual(1, 0)  # ??
-
-    def test_import_errors(self):
-        taxa_file = 'input_data/otu.csv'
-        alpha_file = 'results/alpha_diversity.csv'
-        beta_file = 'results/beta_diversity_coords.csv'
-        df1, df2, df3 = app.parse_csvs(taxa_file, alpha_file, beta_file)
+        diff_file = 'results/differential_abundance_top_20.csv'
+        df1, df2, df3, df4 = app.parse_csvs(taxa_file, alpha_file, beta_file, diff_file)
         # I'll test for sys.exit(0) here if you have a bad file.
+        self.assertEqual(app.get_patient('bad.csv',
+                                         alpha_file,
+                                         beta_file,
+                                         diff_file),
+                                         'Missing Taxa/OTU File')
+        self.assertEqual(app.get_patient(taxa_file,
+                                         'bad.csv',
+                                         beta_file,
+                                         diff_file),
+                                         'Missing alpha diversity File')
+        self.assertEqual(app.get_patient(taxa_file,
+                                         alpha_file,
+                                         'bad.csv',
+                                         diff_file),
+                                         'Missing beta diversity File')
+        self.assertEqual(app.get_patient(taxa_file,
+                                         alpha_file,
+                                         beta_file,
+                                         'bad.csv'),
+                                         'Missing Differential Abundance File')  # noqa
 
     def test_get_patient(self):
         alpha_file = 'results/alpha_diversity.csv'
         df = pd.read_csv(alpha_file)
         self.assertEqual(app.get_patient(df), 'SRR5946632')
-        pass
 
     def test_parse_rf(self):
-        # I want this to parse the rf file for the patient prediction
         self.assertEqual(app.parse_rf_export('rf_report.txt'),
                          'Healthy Control')
 
     def test_parse_rf_errors(self):
-        # test if the file exists/is empty
-        # I want sys.exit(0) for controlled errors
-        pass
+        self.assertEqual(app.parse_rf_export('bad.txt'),
+                         'Cannot find RF_report File!')
