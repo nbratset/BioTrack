407df8c87c6f7845dd6b9bb0e123a51d126e022b
nbratset <nbratset@gmail.com>
2025-12-01 21:36:39 -0700

    added functional app tests

--- test/func/test_pipeline.sh
+++ test/func/test_pipeline.sh
@@ -1,14 +1,21 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_args python src/run_pipeline.py --location Netherlands --otu_file input_data/otu.csv --metadata_file input_data/combined_metadata.csv
+run test_args python src/app.py -n 'John Doe'
 assert_no_error
 
-run test_bad_otu_file python src/run_pipeline.py --location Netherlands --otu_file input_data/bad_file.csv --metadata_file input_data/combined_metadata.csv
+run test_bad_otu_file python src/app.py -n 'John Doe' --taxa_file input_data/bad_file.csv
 assert_exit_code 0
-assert_in_stdout "Missing OTU File"
+assert_in_stdout "Missing Taxa/OTU File"
 
-run test_bad_otu_file python src/run_pipeline.py --location Netherlands --otu_file input_data/otu.csv --metadata_file input_data/bad_file.csv
+run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --alpha_file input_data/bad_file.csv
 assert_exit_code 0
-assert_in_stdout "Missing Metadata File"
+assert_in_stdout "Missing alpha diversity File"
 
+run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --beta_file input_data/bad_file.csv
+assert_exit_code 0
+assert_in_stdout "Missing beta diversity File"
+
+run test_bad_otu_file python src/run_pipeline.py -n 'John Doe' --diff_file input_data/bad_file.csv
+assert_exit_code 0
+assert_in_stdout "Missing Differential Abundance File"
