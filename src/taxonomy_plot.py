c79df0af8b444fdd8843f79ef73254e60e020b22
nbratset <nbratset@gmail.com>
2025-12-02 13:22:17 -0700

    pycodestyle fix

--- src/taxonomy_plot.py
+++ src/taxonomy_plot.py
@@ -37,7 +37,7 @@ def barplot_taxa_facet_fill(otu_table, taxonomy_series, metadata,
     if not np.issubdtype(otu_table.dtypes.values[0], np.number):
         raise ValueError("OTU table must contain numeric values only.")
 
-    # required_ranks = ["Phylum", "Class", "Order", "Family", "Genus", "Species"]
+    # required_ranks = ["Phylum", "Class", "Order", "Family", "Genus", "Species"]  # noqa
     # missing = [rank for rank in required_ranks if rank not in taxonomy_series.index]  # noqa
     # if missing:
     #     raise ValueError(f"Taxonomy table is missing required taxonomic ranks: {', '.join(missing)}")  # noqa
